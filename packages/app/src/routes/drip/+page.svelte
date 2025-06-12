7114429908bfc24fc4629f9905334c328688ebeb
2qx <2qx.git@small.neomailbox.ch>
2025-06-12 09:48:41 -0500

    update

--- packages/app/src/routes/drip/+page.svelte
+++ packages/app/src/routes/drip/+page.svelte
@@ -2,19 +2,23 @@
 	import { encodeTransactionBCH, binToHex } from '@bitauth/libauth';
 	import { blo } from 'blo';
 	// Import library features.
-	import { ElectrumClient } from '@electrum-cash/network';
+	import { ElectrumClient, ConnectionStatus } from '@electrum-cash/network';
 	import { Drip } from '@unspent/drip';
 	import { onMount, onDestroy } from 'svelte';
 
+	import CONNECTED from '$lib/images/connected.svg';
+	import DISCONNECTED from '$lib/images/disconnected.svg';
 	let unspent: any[] = [];
 	let electrumClient: any;
 	let scripthash = Drip.getScriptHash();
 	let contractState = '';
+	let connectionStatus = '';
 
 	const handleNotifications = function (data: any) {
 		if (data.method === 'blockchain.scripthash.subscribe') {
 			if (data.params[1] !== contractState) {
 				contractState = data.params[1];
+				connectionStatus = ConnectionStatus[electrumClient.status];
 				updateUnspent();
 			}
 		} else {
@@ -29,7 +33,8 @@
 		response as any[];
 	};
 
-	const processOutput = async function (utxo: any) {
+	const processOutput = async function (utxo: any, index: number) {
+		unspent.splice(index, 1);
 		let txn = Drip.processOutpoint(utxo);
 		let raw_tx = binToHex(encodeTransactionBCH(txn));
 		console.log(raw_tx);
@@ -74,11 +79,26 @@
 </svelte:head>
 
 <section>
+	<div class="status">
+		{#if connectionStatus == 'CONNECTED'}
+			<img src={CONNECTED} alt={connectionStatus} />
+		{:else}
+			<img src={DISCONNECTED} alt="Disconnected" />
+		{/if}
+	</div>
+
 	<div class="grid">
-		{#each unspent as item}
+		{#each unspent as item, index}
 			{#if item.height !== 0}
 				<div class="row">
-					<button onclick={() => processOutput(item)}>
+					<button onclick={() => processOutput(item, index)}>
+						<img src={blo('0x' + item.tx_hash)} alt={item.tx_hash} />
+						<p>{Number(item.value).toLocaleString()}</p>
+					</button>
+				</div>
+				{:else}
+				<div class="row">
+					<button disabled>
 						<img src={blo('0x' + item.tx_hash)} alt={item.tx_hash} />
 						<p>{Number(item.value).toLocaleString()}</p>
 					</button>
@@ -93,6 +113,10 @@
 		justify-content: center;
 		align-items: center;
 		flex: 0.6;
+	}
+
+	.status {
+		text-align: end;
 	}
 
 	.grid {
@@ -114,5 +138,8 @@
 		font-size: x-small;
 		font-weight: 600;
 		margin: 0px;
+	}
+	button:disabled{
+		filter: grayscale(100%);
 	}
 </style>
