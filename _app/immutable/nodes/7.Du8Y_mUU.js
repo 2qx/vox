b6fcd1203663d1f2c0e0fc75e88056bd776d82c3
2qx <2qx@users.noreply.github.com>
2025-09-03 21:06:14 +0000

    deploy: 6991338586d713cb7fd4a135ea68630ce8ba841a

--- _app/immutable/nodes/7.Du8Y_mUU.js
+++
@@ -1,2 +0,0 @@
-import"../chunks/yGPdYsEk.js";import{p as a,a as p,j as r,a0 as n}from"../chunks/4Ej_4qXs.js";import{C as i}from"../chunks/B8WbVGxB.js";const c=({params:t})=>({topic:t.chan,post:{title:`/pop/${t.chan}`}}),_=Object.freeze(Object.defineProperty({__proto__:null,load:c},Symbol.toStringTag,{value:"Module"}));function d(t,e){a(e,!0);let o=n(()=>e.data.topic);i(t,{get topic(){return r(o)}}),p()}export{d as component,_ as universal};
-//# sourceMappingURL=7.Du8Y_mUU.js.map
