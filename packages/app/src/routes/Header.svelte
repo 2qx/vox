5e39e68c8aa6a1648bccf69918d3689543ec62f9
2qx <2qx.git@small.neomailbox.ch>
2026-01-10 19:37:29 -0500

    fix banner

--- packages/app/src/routes/Header.svelte
+++ packages/app/src/routes/Header.svelte
@@ -53,7 +53,6 @@
 	.dev {
 		background-color: rgb(255, 225, 0);
 		font-weight: 900;
-		min-height: 50px;
 		color: rgb(0, 0, 0);
 		text-align: center;
 	}
@@ -62,7 +61,6 @@
 		width: 100%;
 		background-color: rgb(255, 0, 255);
 		font-weight: 900;
-		max-height: 50px;
 		color: white;
 		text-align: center;
 	}
