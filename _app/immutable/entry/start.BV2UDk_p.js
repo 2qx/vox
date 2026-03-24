9d2542e4aec4081690f0294cfc1455a553b38be7
2qx <2qx@users.noreply.github.com>
2026-03-24 20:19:24 +0000

    deploy: 4751071f5c471e842883dacfad977c29d26a760b

---
+++ _app/immutable/entry/start.BV2UDk_p.js
@@ -0,0 +1,2 @@
+import{l as o,b as r}from"../chunks/cYUphgiD.js";export{o as load_css,r as start};
+//# sourceMappingURL=start.BV2UDk_p.js.map
--- _app/immutable/entry/start.CHe6xtLF.js
+++
@@ -1,2 +0,0 @@
-import{l as o,b as r}from"../chunks/CjV5p2Vg.js";export{o as load_css,r as start};
-//# sourceMappingURL=start.CHe6xtLF.js.map
