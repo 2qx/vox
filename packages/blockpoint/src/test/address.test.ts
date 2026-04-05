8bf9542668a5c9ab2c51bb8e8a6fb556b0dfd695
2qx <2qx.git@small.neomailbox.ch>
2026-04-05 09:42:09 -0500

    blockpoint hotfix

--- packages/blockpoint/src/test/address.test.ts
+++ packages/blockpoint/src/test/address.test.ts
@@ -1,20 +1,13 @@
 import test from 'ava';
-import { getHdPrivateKey, TransactionRequest } from "@unspent/tau";
-// @ts-ignore
-import getAnAliceWallet from "../../../../scripts/aliceWallet.js";
-import { RegTestWallet } from "mainnet-js";
 
 import BlockPoint from "../index.js";
 
 
 // Async arrow function
-test('test wrap covenant address', async t => {
-  const alice = await getAnAliceWallet(500_000)
-  //alice.provider = regTest
-  const aliceBalance = await alice.getBalance('sats') as number
-  t.is(aliceBalance, 500000);
+test('test blockpoint covenant address', async t => {
+  
 
-  t.is(BlockPoint.getAddress(), "bitcoincash:r0ak899wsk9mr9wz78tw2pnppumzry6ypejfvsad6u4vrj4kdewpwsmza35av")
+  t.is(BlockPoint.getAddress(), "bitcoincash:r06fp2k6ux4c6dq8dpafmmpapq9julzfpk04k5309zhmu2vw20mh7srlt9cr9")
   t.is(BlockPoint.getAddress("bchreg"), "bchreg:r0ak899wsk9mr9wz78tw2pnppumzry6ypejfvsad6u4vrj4kdewpwxkpfwslk")
   t.is(BlockPoint.getAddress("bchtest"), "bchtest:r0ak899wsk9mr9wz78tw2pnppumzry6ypejfvsad6u4vrj4kdewpwnunrf8g7")
 
--- scripts/airdrop.ts
+++ scripts/airdrop.ts
@@ -5,7 +5,7 @@ import 'dotenv/config';
 
 const wif = process.env.WIF;
 const tokenIdFungible = "7fe0cd5197494e47ade81eb164dcdbd51859ffbe581fe4a818085d56b2f3062c"
-const destination = "bitcoincash:r0ak899wsk9mr9wz78tw2pnppumzry6ypejfvsad6u4vrj4kdewpwsmza35av"
+const destination = "bitcoincash:r06fp2k6ux4c6dq8dpafmmpapq9julzfpk04k5309zhmu2vw20mh7srlt9cr9"
 
 if(!wif || !tokenIdFungible ) throw new Error("missing .env variables")
 
@@ -17,7 +17,7 @@ let requestList = Array(127).fill({
   cashaddr: destination,
   value: 800,
   tokenId: tokenIdFungible as string,
-  amount: 72624976668147841n
+  amount: 72624976668144699n
 } as TokenSendRequest) as TokenSendRequest[]
 
 // console.log(requestList)
