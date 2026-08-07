6ba555e5e56b3ea71e24bc1b9a998a0b238670d9
2qx <2qx.git@small.neomailbox.ch>
2026-08-06 19:14:27 -0500

    update countdown

--- packages/app/src/lib/Countdown.svelte
+++ packages/app/src/lib/Countdown.svelte
@@ -1,5 +1,5 @@
 <script lang="ts">
-	import { tweened } from 'svelte/motion';
+	import { Tween } from 'svelte/motion';
 	import { linear as easing } from 'svelte/easing';
 	import { fly } from 'svelte/transition';
 
@@ -19,11 +19,18 @@
 		now = Date.now();
 	}
 
+
 	let interval = setInterval(updateTimer, 1000);
 	$: if (count === 0) clearInterval(interval);
 
-	let offset = tweened(1, { duration, easing });
-	let rotation = tweened(360, { duration, easing });
+    let offset = new Tween(0, {
+		duration: duration,
+        easing: easing
+	});
+	let rotation = new Tween(360, {
+		duration: duration,
+        easing: easing
+	});
 
 	$: offset.set(Math.max(count - 1, 0) / countdown);
 	$: rotation.set((Math.max(count - 1, 0) / countdown) * 360);
@@ -41,22 +48,22 @@
 		<g fill="none" stroke="currentColor" stroke-width="2">
 			<circle stroke="currentColor" r="46" />
 			<path
-				stroke="hsl(208, 100%, 50%)"
+				stroke="hsl(300, 90%, 50%)"
 				d="M 0 -46 a 46 46 0 0 0 0 92 46 46 0 0 0 0 -92"
 				pathLength="1"
 				stroke-dasharray="1"
-				stroke-dashoffset={$offset}
+				stroke-dashoffset={offset.current}
 			/>
 		</g>
-		<g fill="hsl(208, 100%, 50%)" stroke="none">
-			<g transform="rotate({$rotation})">
+		<g fill="hsl(300, 90%, 50%)" stroke="none">
+			<g transform="rotate({rotation.current})">
 				<g transform="translate(0 -46)">
 					<circle r="4" />
 				</g>
 			</g>
 		</g>
 
-		<g fill="currentColor" text-anchor="middle" dominant-baseline="baseline" font-size="13">
+		<g fill="currentColor" text-anchor="middle" dominant-baseline="baseline" font-size="18">
 			<text x="-3" y="6.5">
 				{#each Object.entries({ h, m, s }) as [key, value], i}
 					{#if countdown >= 60 ** (2 - i)}
--- packages/app/src/routes/energy/+page.svelte
+++ packages/app/src/routes/energy/+page.svelte
@@ -21,6 +21,7 @@
 	import CONNECTED from '$lib/images/connected.svg';
 	import DISCONNECTED from '$lib/images/disconnected.svg';
 	import Countdown from '$lib/Countdown.svelte';
+	import Loading from '$lib/Loading.svelte';
 	import {
 		binToBigIntUint256LE,
 		getHdPrivateKey,
@@ -67,7 +68,7 @@
 	let electrumClient: any;
 	let scripthash = '';
 	scripthash = Photon.getScriptHash();
-	const isMainnet = page.url.hostname == 'vox.cash';
+	const isMainnet = true; // page.url.hostname == 'vox.cash';
 	let server = isMainnet ? 'bch.imaginary.cash' : 'chipnet.bch.ninja';
 	const icon = isMainnet ? PHOTON : tPHOTON;
 	const CATEGORY = isMainnet ? binToHex(PHOTON_CATEGORY) : binToHex(tPHOTON_CATEGORY);
@@ -364,12 +365,18 @@
 			<input type="checkbox" bind:checked={showSettings} />
 			<span class="slider round"></span>
 		</label>
-		<br />
 	{:else if !isMainnet}
-		<button class="button" onclick={() => fundVault()}>Mint Genesis Tx (0.5 {baseTicker})</button>
-	{:else}
+		<button class="button" onclick={() => fundVault()}
+			>Mint Chipnet Genesis Tx (0.5 {baseTicker})</button
+		>
+	{:else if Date.now() < 1786209657000}
 		<div class="swap">
 			<Countdown end={1786209657000} />
+		</div>
+	{:else}
+		<div class="swap">
+			<Loading />
+			<p> awaiting baton</p>
 		</div>
 	{/if}
 
@@ -408,5 +415,67 @@
 		padding: 10px;
 		justify-content: center;
 		text-align: center;
+	}
+
+	.switch {
+		position: relative;
+		display: inline-block;
+		width: 60px;
+		height: 34px;
+	}
+
+	/* Hide default HTML checkbox */
+	.switch input {
+		opacity: 0;
+		width: 0;
+		height: 0;
+	}
+
+	/* The slider */
+	.slider {
+		position: absolute;
+		cursor: pointer;
+		top: 0;
+		left: 0;
+		right: 0;
+		bottom: 0;
+		background-color: #ccc;
+		-webkit-transition: 0.4s;
+		transition: 0.4s;
+	}
+
+	.slider:before {
+		position: absolute;
+		content: '';
+		height: 26px;
+		width: 26px;
+		left: 4px;
+		bottom: 4px;
+		background-color: white;
+		-webkit-transition: 0.4s;
+		transition: 0.4s;
+	}
+
+	input:checked + .slider {
+		background-color: #a45eb6;
+	}
+
+	input:focus + .slider {
+		box-shadow: 0 0 1px #a45eb6;
+	}
+
+	input:checked + .slider:before {
+		-webkit-transform: translateX(26px);
+		-ms-transform: translateX(26px);
+		transform: translateX(26px);
+	}
+
+	/* Rounded sliders */
+	.slider.round {
+		border-radius: 34px;
+	}
+
+	.slider.round:before {
+		border-radius: 50%;
 	}
 </style>
